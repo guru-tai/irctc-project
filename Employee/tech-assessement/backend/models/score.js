564ac57f970e356f4032d131abf66429acaa5274
guru-tai <guruswami@thetapacademy.com>
2024-10-28 12:03:08 +0530

    Remove all files from repository

--- Employee/tech-assessement/backend/models/score.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ScoreSchema = new mongoose.Schema({
-    technology: String,
-    score: Number,
-    remark: String
-});
-
-module.exports = mongoose.model('Score', ScoreSchema);
--- Javascript/date.js
+++
@@ -1,4 +0,0 @@
-function testScope() 
-{
-   let x = 5; if (true)
-     { let x = 10; console.log("Inside if block:", x);
